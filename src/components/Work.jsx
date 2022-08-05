d8a2b35e773928c79144773101097ee6c93c11be
datomaluta <datomaluta@gmail.com>
2022-08-05 20:13:35 +0400

    add casino project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,6 +5,7 @@ import fork from "../assets/fork.jpg";
 import farm from "../assets/farm.jpg";
 import omni from "../assets/omni.jpg";
 import bankist from "../assets/bankist.jpg";
+import casino from "../assets/casino.jpg";
 
 const Work = () => {
   return (
@@ -20,6 +21,29 @@ const Work = () => {
         {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${casino})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://casinomalia.netlify.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/datomaluta/Casino.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
           <div
             style={{ backgroundImage: `url(${food})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
